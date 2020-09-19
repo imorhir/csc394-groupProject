e2a1724b212766e0c93ec2bbd50c09f06717b4c1
Issam Morhir <imorhir@mail.depaul.edu>
2020-09-19 03:37:25 -0500

    first commit spring boot starter

---
+++ src/main/java/com/csc394/capStoneProject/CapStoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csc394.capStoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapStoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csc394/capStoneProject/CapStoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc394.capStoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
