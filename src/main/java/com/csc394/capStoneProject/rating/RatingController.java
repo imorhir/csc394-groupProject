0b1af66ec681b2ecf7e41a2a651f3e35023de0b3
Mikala Metzger <mmetzgerdpu2021@gmail.com>
2020-10-18 17:18:38 -0500

    Added basic team and rating controller methods.

--- src/main/java/com/csc394/capStoneProject/rating/RatingController.java
+++ src/main/java/com/csc394/capStoneProject/rating/RatingController.java
@@ -1,6 +1,8 @@
 package com.csc394.capStoneProject.rating;
 
 import com.csc394.capStoneProject.rating.RatingService;
+import com.csc394.capStoneProject.team.Team;
+import com.csc394.capStoneProject.team.TeamRepository;
 import org.apache.tomcat.jni.Address;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +17,35 @@ public class RatingController {
     @Autowired
     RatingService ratingService;
 
+    private final RatingRepository ratingRepo;
+
+    RatingController(RatingRepository ratingRepo) {
+        this.ratingRepo = ratingRepo;
+    }
+
     @GetMapping("/ratings")
     public String getRatings(Model model) {
         Iterable<Rating> ratings = ratingService.getAllRatings();
         model.addAttribute("ratings", ratings);
 
         return "rating";
+    }
+
+    // get all ratings
+    @GetMapping("/ratings")
+    List<Rating> all() {
+        return ratingRepo.findAll();
+    }
+
+    // add new rating
+    @PostMapping("/ratings")
+    Rating newRating(@RequestBody Rating newRating) {
+        return ratingRepo.save(newRating);
+    }
+
+    // delete rating
+    @DeleteMapping("/ratings/{id}")
+    void deleteRating(@PathVariable Long id) {
+        ratingRepo.deleteById(id);
     }
 }
--- src/main/java/com/csc394/capStoneProject/team/TeamController.java
+++ src/main/java/com/csc394/capStoneProject/team/TeamController.java
@@ -2,6 +2,7 @@ package com.csc394.capStoneProject.team;
 
 import com.csc394.capStoneProject.rating.Rating;
 import com.csc394.capStoneProject.rating.RatingService;
+import com.csc394.capStoneProject.user.User;
 import org.apache.tomcat.jni.Address;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,35 @@ public class TeamController {
     @Autowired
     TeamService teamService;
 
+    private final TeamRepository teamRepo;
+
+    TeamController(TeamRepository teamRepo) {
+        this.teamRepo = teamRepo;
+    }
+
     @GetMapping("/teams")
     public String getTeams(Model model) {
         Iterable<Team> teams = teamService.getAllTeams();
         model.addAttribute("teams", teams);
 
         return "team";
+    }
+
+    // get all teams
+    @GetMapping("/teams")
+    List<Team> all() {
+        return teamRepo.findAll();
+    }
+
+    // add new team (member)
+    @PostMapping("/teams")
+    Team newTeam(@RequestBody Team newTeam) {
+        return teamRepo.save(newTeam);
+    }
+
+    // delete team (and members)
+    @DeleteMapping("/teams/{id}")
+    void deleteTeam(@PathVariable Long id) {
+        teamRepo.deleteById(id);
     }
 }
