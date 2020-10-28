9a7b287ab927ca91afe4ce41e4df005a7249831d
Mikala Metzger <mmetzgerdpu2021@gmail.com>
2020-10-27 20:11:47 -0500

    Commented out bad SQL.

--- src/main/resources/db/migration/V5.0_teams_table.sql
+++ src/main/resources/db/migration/V5.0_teams_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `teams` (
-
-	`team_id` BIGINT AUTO_INCREMENT,
-	`student_id` BIGINT,
-	`faculty_id` BIGINT,
-	PRIMARY KEY (`team_id`, `student_id`),
-    CONSTRAINT team_student_fk FOREIGN KEY(`student_id`) REFERENCES users(`user_id`),
-    CONSTRAINT team_faculty_fk FOREIGN KEY(`faculty_id`) REFERENCES users(`user_id`)
-);
+--CREATE TABLE `teams` (
+--
+--	`team_id` BIGINT AUTO_INCREMENT,
+--	`student_id` BIGINT,
+--	`faculty_id` BIGINT,
+--	PRIMARY KEY (`team_id`, `student_id`),
+--    CONSTRAINT team_student_fk FOREIGN KEY(`student_id`) REFERENCES users(`user_id`),
+--    CONSTRAINT team_faculty_fk FOREIGN KEY(`faculty_id`) REFERENCES users(`user_id`)
+--);
